7ae11bbe52258cff636579120a150c5342ccdb94
obiwandre <andreojtj@gmail.com>
2024-06-30 18:57:50 -0300

    url no setup.py corrigida

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description='Biblioteca de instalação de módulos privados',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='URL do seu projeto (por exemplo, no GitHub)',
+    url='git@github.com:obiwandre/obiwandre.git',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
