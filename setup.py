c973bf22496efa97309b114c916f7621bb122439
obiwandre <andreojtj@gmail.com>
2024-06-30 19:10:54 -0300

    uel

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description='Biblioteca de instalação de módulos privados',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/obiwandre/obiwandre.git',
+    url='https://github.com/obiwandre/obiwandre',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
