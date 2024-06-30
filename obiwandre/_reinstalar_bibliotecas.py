93679defb9fc53922a8db6191f3b7b6acb3ae138
obiwandre <andreojtj@gmail.com>
2024-06-30 19:50:11 -0300

    nomenclatura

--- obiwandre/_reinstalar_bibliotecas.py
+++ obiwandre/_reinstalar_bibliotecas.py
@@ -12,5 +12,9 @@ def pip_reinstalar_bumblebee():
     package_url = f"https://{GITHUB_USER}:{GITHUB_TOKEN}@github.com/{GITHUB_USER}/bumblebee.git@main" 
     subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "--force-reinstall", f"git+{package_url}"])
 
+def pip_reinstalar_sauron():
+    package_url = f"https://{GITHUB_USER}:{GITHUB_TOKEN}@github.com/{GITHUB_USER}/sauron.git@main" 
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "--force-reinstall", f"git+{package_url}"])
+
 
 
